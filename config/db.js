6fd78901f2755552ef9f1f69e913b3c74ffd75bc
Ash <akash.makam.am@gmail.com>
2018-08-12 16:53:03 +0530

    fixed a silly mistake

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
-if (process.env.NODE_ENV === 'developement') {
+if (process.env.NODE_ENV === 'production') {
   module.exports =  {MONGO_URI: process.env.ABOUT_URI}
 } else {
   module.exports = {MONGO_URI: 'mongodb://localhost:27017/AboutMe'}
 }
+
+console.log(process.env.NODE_ENV);
